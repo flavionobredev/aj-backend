ccf8ce96443f506fdb6d314e8c58dd6c577c2c76
Flavio Nobre <flavionobredev@gmail.com>
2022-04-25 21:37:50 -0300

    refactor: mudando nome da rota de healthcheck

--- src/main/routes/general.ts
+++ src/main/routes/general.ts
@@ -3,7 +3,7 @@ import { makeAppHealthCheck } from '../factories/healthcheck/app-healthcheck';
 const healthCheck = makeAppHealthCheck();
 export default (router: Router) => {
   router.get('/', (_, res) => res.status(200).json({ message: 'ok' }));
-  router.get('/health', async (_, res) => {
+  router.get('/_health', async (_, res) => {
     const response = await healthCheck.check();
     return response
       ? res.status(200).json({ message: 'healthy' })
